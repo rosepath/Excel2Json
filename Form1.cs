c9f4e3b2cbcc9c974131df6a5c1a9b03296ccf87
saran <saran@yahoo.co.in>
2017-10-31 12:45:02 +0530

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel2Json
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel2JsonConverter c = new Excel2JsonConverter();
+            try
+            {
+                c.ExcelFileToJson(textBox1.Text);
+
+                label1.Text = "Converted !!!";
+            }
+            catch(Exception ex)
+            {
+                label1.Text = "Error:" + ex.Message;
+            }
+        }
+    }
+}
